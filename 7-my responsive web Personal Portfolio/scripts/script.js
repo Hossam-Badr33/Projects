b1d24f524159433c35d0cb0718015bdb58cf6abe
Hossam-Badr33 <dream.onlinebusiness@gmail.com>
2022-07-27 12:48:08 +0300

    change design divControl, fix some issues in .side

--- 7-my responsive web Personal Portfolio/scripts/script.js
+++ 7-my responsive web Personal Portfolio/scripts/script.js
@@ -9,21 +9,57 @@ $(document).ready(function () {
   //.control-color-light maximize and minimize
   let divControl = $(".control-color-light");
   let control = $(".fa-control i");
+  let scrollEvent = false;
   divControl.animate({
     right: -$(".control-color-container").innerWidth(),
   });
   control.on("click", function () {
     divControl.toggleClass("reduce-width");
+    checkReduceClass();
+  });
+  /*
+  window.addEventListener("scroll", function () {
+    document.getElementByTagName('body')[0].stopScroll();
+    scrollEvent = true;
+    if (scrollEvent) {
+      divControl.hasClass("reduce-width")
+        ? console.log("yes")
+        : divControl.addClass("reduce-width");
+      checkReduceClass();
+    }
+  });
+  */
+  /*
+  $(window).on("scrollstop",function () {
+    console.log("stopped")
+  })
+  */
+  /*
+  console.log($("body")[0]);
+  $(window).on("scroll", function () {
+    scrollEvent = true;
+    if (scrollEvent) {
+      divControl.hasClass("reduce-width")
+        ? console.log("yes")
+        : divControl.addClass("reduce-width");
+      checkReduceClass();
+    }
+  });
+  */
+  function checkReduceClass() {
     if (divControl.hasClass("reduce-width")) {
-      $(divControl).animate({
+      divControl.animate({
         right: -$(".control-color-container").innerWidth(),
       });
     } else {
-      $(divControl).animate({
+      divControl.animate({
         right: 0,
       });
     }
-  });
+    scrollEvent = false;
+  }
+  // $(window).scrollTop(0);
+
   //change moon and sun for page
   let light = $(".fa-light i");
   light.click(function () {
@@ -69,7 +105,6 @@ $(document).ready(function () {
       });
     }
   });
-  $(window).scrollTop(0);
   navItem.on("click", function (e) {
     e.preventDefault();
     $("." + $(this).data("scroll"))
@@ -93,37 +128,78 @@ $(document).ready(function () {
         .find("a")
         .css("color", "");
     }
-    if ($(this).hasClass("color-scheme")) {
-      $("." + $(this).data("scroll"))
-        .siblings()
-        .animate(
-          {
-            left: $(".main-page").innerWidth(),
-          },
-          200
-        );
-      $("." + $(this).data("scroll")).animate(
-        {
-          left: 0,
-        },
-        1000
-      );
+    if (
+      $(this).hasClass("color-scheme") &&
+      hide.css("display") === "none"
+      // &&
+      // $(".fake-class").css("display") === "none"
+    ) {
+      navitemLeft.call(this);
+    }
+    // if (
+    //   $(this).hasClass("color-scheme") &&
+    //   hide.css("display") !== "none" &&
+    //   $(".fake-class").css("display") === "block"
+    // )
+    else {
+      $(".aside").css("left", 0);
+      navitemLeft.call(this);
+      toggleAside();
     }
   });
-  //.hide
-  let hide = $(".main-page .hide");
+  //.aside .hide
+  let hide = $(".aside .hide");
   let aside = $(".aside");
   let faClosing = $(".fa-closing");
-  hide.css({ top: hide.offset({ top: 15.2 }) });
-  hide.on("click", function () {
+  hide.on("click", toggleAside);
+
+  function navitemLeft() {
+    $("." + $(this).data("scroll"))
+      .siblings()
+      .animate(
+        {
+          left: $(".main-page").innerWidth(),
+        },
+        200
+      );
+    $("." + $(this).data("scroll")).animate(
+      {
+        left: 0,
+      },
+      1000
+    );
+  }
+  function toggleAside() {
     aside.toggleClass("active-aside");
-    if (aside.hasClass("active-aside")) {
-      aside.animate({ left: 0 }, { duration: 300, queue: false });
+    if (
+      aside.hasClass("active-aside") &&
+      $(".fake-class").css("display") === "block"
+    ) {
+      aside.animate({ left: 0 }, { duration: 600, queue: false });
       faClosing.fadeIn();
-    } else {
-      aside.animate({ left: "-300px" }, { duration: 300, queue: false });
+    } else if (
+      aside.hasClass("active-aside") === false &&
+      $(".fake-class").css("display") === "block"
+    ) {
+      aside.animate({ left: "-300px" }, { duration: 600, queue: false });
       faClosing.fadeOut();
+    } else if (
+      (aside.hasClass("active-aside") === false ||
+        aside.hasClass("active-aside") === true) &&
+      $(".fake-class").css("display") === "none"
+    ) {
+      console.log("case 3");
+    }
+  }
+  $(window).resize(function () {
+    if (
+      (aside.hasClass("active-aside") === false ||
+        aside.hasClass("active-aside") === true) &&
+      $(".fake-class").css("display") === "none"
+    ) {
+      $(".aside").css("left", 0);
+    } else {
+      $(".aside").css("left", "");
     }
   });
-  console.log(hide.offset().top);
 });
